30201a7a59544da2e5a93abfbbbce12bc32f4378
DarkIISouls <DarkIISouls@outlook.com>
2018-04-10 21:07:15 +0300

    1

---
+++ src/java/entity/Remander.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+
+   
+public class Remander implements Serializable{
+     private static final long serialVersionUID = 34L;
+    private int id;
+    private String remander;
+    
+    private Date date;
+ 
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRemander() {
+        return remander;
+    }
+
+    public void setRemander(String remander) {
+        this.remander = remander;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+}
